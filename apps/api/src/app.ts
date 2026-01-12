eb803a922bbbfa2849e1bea69cede3b812cdb18d
SatyamKumarCS <satyamkumarch15@gmail.com>
2026-01-12 11:14:50 +0530

    feat(core): add global error handler and env setup

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,8 +1,11 @@
 import express, {Request,Response} from 'express';
 import routes from './routes/index.js';
+import { errorHandler } from './middleware/error.middleware.js';
+
 const app = express();
 
 app.use(express.json());
 app.use(routes)
+app.use(errorHandler);
 
 export default app;
